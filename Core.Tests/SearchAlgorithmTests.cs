ad520c4e0ebf4359793aa1f84666549bcf397b42
henry <jaohaohsuan@gmail.com>
2013-04-26 00:31:59 +0800

    Add Source files

---
+++ Core.Tests/SearchAlgorithmTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Core.Tests
+{
+    [TestFixture]
+    public class SearchAlgorithmTests
+    {
+        [Test]
+        public void IsolateShuoldBeFindAssertTrueTest()
+        {
+            var source = new Dictionary<int, bool>();
+
+            source[-4] = true;
+            source[-3] = false;
+            source[-2] = false;
+            source[-1] = true;
+            source[0] = false; //*
+            source[1] = false;  //*
+            source[2] = false;  //*
+            source[3] = true;
+            source[4] = true;
+            source[5] = true;
+            source[6] = true;
+
+            Func<int,bool> getValue = i => source.ContainsKey(i) ? source[i] : false;
+
+            var result = SearchAlgorithm.HasIsolate(getValue, 0);
+            Assert.IsTrue(result);
+
+            result = SearchAlgorithm.HasIsolate(getValue, 1);
+            Assert.IsTrue(result);
+
+            result = SearchAlgorithm.HasIsolate(getValue, -1);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsolateShuoldBeFindAssertFalseTest()
+        {
+            var source = new Dictionary<int, bool>();
+
+            source[-4] = true;
+            source[-3] = false;
+            source[-2] = false;
+            source[-1] = true;
+            source[0] = false; //*
+            source[1] = false;  //*
+            source[2] = false;  //*
+            source[3] = true;
+            source[4] = true;
+            source[5] = true;
+            source[6] = true;
+
+            var result = SearchAlgorithm.HasIsolate(i => source[i], 2);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ReportIsolatePostionIsMinus1AssertTrueTest()
+        {
+            var source = new Dictionary<int, bool>();
+
+            source[-4] = true;
+            source[-3] = false;
+            source[-2] = false;
+            source[-1] = true;
+            source[0] = false; //*
+            source[1] = false;  //*
+            source[2] = false;  //*
+            source[3] = true;
+            source[4] = true;
+            source[5] = true;
+            source[6] = true;
+
+            var result = SearchAlgorithm.ReportIsolate(i => source[i], 1);
+            Assert.IsTrue(result.Contains(-1));
+        }
+    }
+}
---
+++ Core/SearchAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public static class SearchAlgorithm
+    {
+        public static bool HasIsolate(Func<int, bool> notEmpty, int checkPoint)
+        {
+            return ReportIsolate(notEmpty, checkPoint).Any();
+        }
+
+        public static IEnumerable<int> ReportIsolate(Func<int, bool> notEmpty, int checkPoint)
+        {
+            var isolateWorkMask = new[] { false, false, true, false, false };
+
+            var cells = new bool[isolateWorkMask.Length];
+
+            var lastElementIndex = isolateWorkMask.Length - 1;
+
+            var stop = checkPoint + lastElementIndex;
+
+            while (checkPoint != stop)
+            {
+                var start = checkPoint - lastElementIndex;
+                var end = checkPoint;
+
+                var negCorrection = -start;
+                for (int i = start; i < end; i++)
+                    cells[i + negCorrection] = notEmpty(i);
+
+                if (!cells.Where((t, i) => t != isolateWorkMask[i]).Any())
+                    yield return start + 2;//results.Add(start+2); //2 is center position
+               
+                checkPoint++;
+            }
+        }
+    }
+}
